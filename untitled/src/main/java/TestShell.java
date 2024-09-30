c67bd63e4b8885efc2fbbc410e057247f875418a
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-09-30 11:27:49 +0900

    first shell commit

--- untitled/src/main/java/TestShell.java
+++ untitled/src/main/java/TestShell.java
@@ -1,2 +1,19 @@
-public class TestShell {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str;
+        while(true) {
+            System.out.print("SHELL > ");
+            Scanner sc = new Scanner(System.in);
+            str = sc.next();
+
+            if (str.equals("exit")) {
+                System.out.println("BYE");
+                break;
+            }
+        }
+
+    }
 }
